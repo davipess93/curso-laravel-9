6541c8e34fddee0263ba71ec2c64972760683059
Davi Pessoa <davialvespessoa@gmail.com>
2022-08-25 00:06:29 -0300

    Trabalhando com templates blade

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.app")
+
+@section("title", "Listagem dos Usuários")
+
+@section("content")
+  <h1>Listagem dos usuários</h1>
+
+  <ul>
+    @foreach ($users as $user)
+      <li>
+        {{ $user->name }} - {{ $user->email }} | <a href="{{ route("users.show", $user->id) }}">Detalhes</a>
+      </li>  
+    @endforeach
+  </ul>
+@endsection
