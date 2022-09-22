efc79ed20dda8dd14ee97b21a4a8ab44a8fd7b6b
Davi Pessoa <davialvespessoa@gmail.com>
2022-09-21 22:35:15 -0300

    Criando model e tabela Comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,83 +6,85 @@ use App\Http\Requests\StoreUpdateUserFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller {
+class UserController extends Controller
+{
 
-    protected $model;
+  protected $model;
 
-    public function __construct(User $user) {
-        $this->model = $user;
-    }
+  public function __construct(User $user)
+  {
+    $this->model = $user;
+  }
 
-    public function index(Request $request) {
+  public function index(Request $request)
+  {
 
-        $users = $this->model->getUsers(search: $request->search ?? "");
+    $users = $this->model->getUsers(search: $request->search ?? "");
 
-        return view("users.index", compact("users"));
-    }
+    return view("users.index", compact("users"));
+  }
 
-    public function show($id) {
+  public function show($id)
+  {
 
-        // $user = $this->model->where("id", $id)->first();
-        if(!$user = $this->model->find($id))
-            return redirect()->route("users.index");
+    // $user = $this->model->where("id", $id)->first();
+    if (!$user = $this->model->find($id))
+      return redirect()->route("users.index");
 
-        return view("users.show", compact("user"));
+    return view("users.show", compact("user"));
+  }
 
-    }
+  public function create()
+  {
 
-    public function create() {
+    return view("users.create");
+  }
 
-        return view("users.create");
+  public function store(StoreUpdateUserFormRequest $request)
+  {
 
-    }
+    $data = $request->all();
 
-    public function store(StoreUpdateUserFormRequest $request) {
+    $data["password"] = bcrypt($data["password"]);
 
-        $data = $request->all();
+    $this->model->create($data);
 
-        $data["password"] = bcrypt($data["password"]);
+    return redirect()->route("users.index");
+  }
 
-        $this->model->create($data);
+  public function edit($id)
+  {
 
-        return redirect()->route("users.index");
+    if (!$user = $this->model->find($id))
+      return redirect()->route("users.index");
 
-    }
+    return view("users.edit", compact("user"));
+  }
 
-    public function edit($id) {
-        
-        if(!$user = $this->model->find($id))
-            return redirect()->route("users.index");
+  public function update(StoreUpdateUserFormRequest $request, $id)
+  {
 
-        return view("users.edit", compact("user"));
+    if (!$user = $this->model->find($id))
+      return redirect()->route("users.index");
 
-    }
+    $data = $request->only("name", "email");
 
-    public function update(StoreUpdateUserFormRequest $request, $id) {
+    if ($request->password)
+      $data["password"] = $request->password;
 
-        if(!$user = $this->model->find($id))
-            return redirect()->route("users.index");
+    $user->update($data);
 
-        $data = $request->only("name", "email");
+    return redirect()->route("users.index");
+  }
 
-        if($request->password)
-            $data["password"] = $request->password;
+  public function delete($id)
+  {
 
-        $user->update($data);
+    if (!$user = $this->model->find($id))
+      return redirect()->route("users.index");
 
-        return redirect()->route("users.index");
+    $user->delete();
 
-    }
-
-    public function delete($id) {
-
-        if(!$user = $this->model->find($id))
-            return redirect()->route("users.index");
-
-        $user->delete();
-
-        return redirect()->route("users.index");
-
-    }
-
+    return redirect()->route("users.index");
+  }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,50 +10,55 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'email_verified_at' => 'datetime',
+  ];
 
-    public function getUsers(string|null $search = null) {
+  public function getUsers(string|null $search = null)
+  {
 
-        $users = $this->where(function($query) use($search) {
-            
-            if($search) {
-                $query->where("email", $search);
-                $query->orWhere("name", "LIKE", "%{$search}%");
-            }
+    $users = $this->where(function ($query) use ($search) {
 
-        })->get();
+      if ($search) {
+        $query->where("email", $search);
+        $query->orWhere("name", "LIKE", "%{$search}%");
+      }
+    })->get();
 
-        return $users;
+    return $users;
+  }
 
-    }
+  public function comments()
+  {
+    return $this->hasMany(Comment::class);
+  }
+
 }
